bfd77e6b6d259eb247971887676b94e7c7d59733
Tapsman <tapiwanyoni140@gmail.com>
2024-03-11 14:19:39 -0700

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,18 +5,18 @@
  * @list: The linked list to check in
  * Return: 1 if list has cycle, 0 on not
  */
-int check_cycle(listint_t *t)
+int check_cycle(listint_t *list)
 {
 	listint_t *steady = list;
 	listint_t *rapid = list;
 
 	if (!list)
 		return (0);
-	while (steady && rapid && rappid->next)
+	while (steady && rapid && rapid->next)
 	{
 		steady = steady->next;
 		rapid = rapid->next->next;
-		if (steady == fast)
+		if (steady == rapid)
 			return (1);
 	}
 	return (0);
