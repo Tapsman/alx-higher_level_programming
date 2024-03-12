a616d09a9e5208ef974b67ec94c80be80bbc6e73
Tapsman <tapiwanyoni140@gmail.com>
2024-03-12 09:20:18 -0700

    lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -10,11 +10,11 @@
  */
 typedef struct listint_s
 {
-        int n;
-        struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
-#endifi
+#endif
