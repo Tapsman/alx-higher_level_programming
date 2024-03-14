8561ba5e357b4fe84e90ec594caa061086ddc568
Tapsman <tapiwanyoni140@gmail.com>
2024-03-14 13:09:54 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
-    """Python function that matches the bytecode provided"""
+    """Python function that matches the bytecode provided."""
     from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return(c)
+        return (c)
     else:
         return(sub(a, b))
 
