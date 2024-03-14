33db8cb45ee924bbfedfabae26b7401204d063f0
Tapsman <tapiwanyoni140@gmail.com>
2024-03-14 11:49:26 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     count = len(sys.argv) - 1
 
     if count == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
