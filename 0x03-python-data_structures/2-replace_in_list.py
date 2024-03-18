66d2630b52dd9dba4170ea6e0958edd1525eb536
Tapsman <tapiwanyoni140@gmail.com>
2024-03-18 07:58:54 -0700

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        elif idx > len(my_list) - 1:
+            else:
+                my_list[idx] = element
+                return my_list
