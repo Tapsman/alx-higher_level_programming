6a4a6ca584377a7af970265154a092de44d64074
Tapsman <tapiwanyoni140@gmail.com>
2024-03-20 16:35:11 -0700

    5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    number = 0
+    list_keys = list(a_dictionary.keys())
+    for i in list_keys:
+        number = number + 1
+    return (number)
