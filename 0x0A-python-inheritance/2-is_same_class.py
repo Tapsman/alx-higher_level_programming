7a8deee647739cde4d2122ec549b1999f93c8c57
Tapsman <tapiwanyoni140@gmail.com>
2024-04-15 11:21:34 -0700

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is the module for is_same_close meth."""
+
+
+def is_same_class(obj, a_class):
+    """Dtermines if an object is exactly an instance of a class."""
+    return type(obj) == a_class
