830ca76ae2e962aacc9837e85298dae1c852c490
Tapsman <tapiwanyoni140@gmail.com>
2024-04-15 11:57:50 -0700

    5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This is the module for BaseGeomentry."""
+
+
+class BaseGeometry:
+    """A BaseGeometry class."""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This is the module for BaseGeomentry."""
+
+
+class BaseGeometry:
+    """A BaseGeometry class."""
+    def area(self):
+        """Computes the area."""
+        raise Exception('area() is implemented')
