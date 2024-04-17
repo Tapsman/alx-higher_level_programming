4caeb1d218b5fae450b663e094e98beb2561dcf7
Tapsman <tapiwanyoni140@gmail.com>
2024-04-17 12:28:41 -0700

    5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,4 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Funct to write and object to text file using JSON repreentation."""
-    with open(filename, :w:) as f:
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,6 +13,8 @@ def __init__(self, first_name, last_name, age):
     self.first_name = first_name
     self.last_name = last_name
     self.age = age
+
+
 def to_json(self):
     """Get a dictionary representation of the Student."""
     return self
