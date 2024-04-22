d40eecafc1caeaaa382820ecfaab0aa179213e2e
Tapsman <tapiwanyoni140@gmail.com>
2024-04-22 11:43:44 -0700

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(Base):
     """Rectangle class."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """The constructer."""
+        """The constructor."""
         super().__init__(id)
         self.width = width
         self.height = height
