6f5780b9e077e791256d52a6eb17b6bbba5321db
Tapsman <tapiwanyoni140@gmail.com>
2024-04-22 11:14:28 -0700

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -72,12 +72,12 @@ class Base:
         if list_objs is not None:
             if cls is Rectangle:
                 list_objs = [[o.id, o.width, o.height, o.x, o.y]
-                        for o in list_objs]
+                             for o in list_objs]
             else:
                 list_objs = [[o.id, o.size, o.x, o.y]
-                        for o in list_objs]
+                             for o in list_objs]
         with open('{}.csv'.format(cls.__name__), 'w', newline='',
-                encoding='utf-8') as f:
+                  encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerows(list_objs)
 
@@ -88,16 +88,16 @@ class Base:
         from models.square import Square
         ret = []
         with open('{}.csv'.format(cls.__name__), 'r', newline='',
-                encoding='utf-8') as f:
+                  encoding='utf-8') as f:
             reader = csv.reader(f)
             for row in reader:
                 row = [int(r) for r in row]
                 if cls is Rectangle:
                     d = {"id": row[0], "width": row[1], "height": row[2],
-                            "x": row[3], "y": row[4]}
+                         "x": row[3], "y": row[4]}
                 else:
                     d = {"id": row[0], "size": row[1],
-                            "x": row[2], "y": row[3]}
+                         "x": row[2], "y": row[3]}
                 ret.append(cls.create(**d))
         return ret
 
