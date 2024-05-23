d57e6847744f6330d1854de3851755ee1b2d5570
Tapsman <tapiwanyoni140@gmail.com>
2024-05-23 13:58:32 -0700

    9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- A script that a table second_table on hbtn_0c_0 SQL Server
+CREATE TABLE IF NOT EXISTS second_table (id, INT, name VARCHAR(256), score INT);
+
+-- The script prints 4 records
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (1, "John", 10);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (2, "Alex", 3);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (3, "Bob", 14);
+INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (4, "George", 8);
