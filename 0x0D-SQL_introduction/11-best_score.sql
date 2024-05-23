acf6d317c13af00d97c2ed2707cb0b014ef2d87e
Tapsman <tapiwanyoni140@gmail.com>
2024-05-23 14:36:25 -0700

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- List all the records with score >=10
-SELECT score, name FROM second_table WHERE score >= 10 ORDER score BY DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
