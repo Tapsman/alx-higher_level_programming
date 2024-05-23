ccc581bbb04721ebb86821fa5a57a8a1988c907b
Tapsman <tapiwanyoni140@gmail.com>
2024-05-23 15:15:53 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- This is a script that creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'hbtn_0d_2'@'localhost' IDENTIFIED BY 'hbtn_0d_2_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'hbtn_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'hbtn_0d_2'@'localhost';
 FLUSH PRIVILEGES;
