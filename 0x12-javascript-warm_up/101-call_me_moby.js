ae070eaf3e5501d64d8236285b567bcf75d96e3c
Tapsman <tapiwanyoni140@gmail.com>
2024-06-18 16:29:01 -0700

    101-call_me_moby.js

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* This is a function to execute x times a function */
+
+expots.callMeMoby = function (x, theFunction) {
+  let i = 0;
+  for (i = 0; i < x; i++) {
+    theFunction();
+  }
+};
