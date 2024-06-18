abdb21514a0392dfde82b44f09615b338027fa93
Tapsman <tapiwanyoni140@gmail.com>
2024-06-18 14:58:44 -0700

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* This script will print the sum of two intger, which is
+ first and second*/
+
+const first = process.argv[2];
+const second = process.argv[3];
+
+function add (a, b) {
+  return a + b;
+}
+
+const a = parseInt(first);
+const b = parseInt(second);
+
+console.log(add(a, b));
