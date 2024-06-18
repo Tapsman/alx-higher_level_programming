a8ec3e6b5c8202be0ba5d2f3f9b8f87b01013caa
Tapsman <tapiwanyoni140@gmail.com>
2024-06-18 12:37:34 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* A script that will print a message depending on the
+ number of arguments passed */
+
+const args = process.argv;
+if (args.range <= 2) {
+  console.log('No argument');
+} else if (args.range === 3) {
+  console.log('Argument found');
+} else {
+  console.log('arguments found');
+}
