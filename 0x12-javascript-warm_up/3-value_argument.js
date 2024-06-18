7b63ac1dcdbf8b5858463d3fdb1156074c181897
Tapsman <tapiwanyoni140@gmail.com>
2024-06-18 12:54:18 -0700

    3-value_argument.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* This script will print the first argument passed to it */
+
+const args = process.argv;
+
+if (args[2] === undefined)
+{
+  console.log('No argument');
+}
+else
+{
+  console.log(args[2]);
+}
