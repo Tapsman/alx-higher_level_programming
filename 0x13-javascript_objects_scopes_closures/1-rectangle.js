9795d1239d8643049ea4c3c23490777b37ab4739
Tapsman <tapiwanyoni140@gmail.com>
2024-06-19 09:49:03 -0700

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* A class that defines a rectangle */
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
