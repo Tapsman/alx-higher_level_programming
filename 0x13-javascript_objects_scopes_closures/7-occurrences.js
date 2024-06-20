61def4ed112db0323b9c532ffaaa362af724d504
Tapsman <tapiwanyoni140@gmail.com>
2024-06-20 12:44:35 -0700

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,6 +2,6 @@
 /** This is the a function that returns the number of
  * occurrences in a list */
 
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   return list.reduce((a, z) => (z === searchElement ? a + 1 : a), 0);
 }
