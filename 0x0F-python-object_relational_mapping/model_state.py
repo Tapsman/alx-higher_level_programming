3f213f80bf8c3e51ef69769537a20d9270880417
Tapsman <tapiwanyoni140@gmail.com>
2024-06-24 11:58:51 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,15 @@
-"""The script will create the database"""
-CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
-USE hbtn_0e_6_usa;
-SHOW CREATE TABLE states;
+#!/usr/bin/python3
+"""
+This is a python file that contains the class defination of a state
+and an instance base = declarative_base():
+"""
+
+import sys
+from model_state import Base, State
+
+from sqlelchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
