32cf622bc3ba2236f8d164925a05cea4d8111b72
Tapsman <tapiwanyoni140@gmail.com>
2024-07-04 09:30:50 -0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/bash
 # This is a bash script that takes a URL, sends a request
 # to that URL and then displays the size of the body
-curl - s "$1" | wc - c
+curl -s "$1" | wc -c
