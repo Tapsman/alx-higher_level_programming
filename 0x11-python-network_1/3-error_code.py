642bdf283dbccfac3bff7e06c493492a662ead8f
Tapsman <tapiwanyoni140@gmail.com>
2024-07-05 08:41:24 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
 
     try:
         with request.urlopen(sys.argv[1]) as response:
-            print(response.read()decode('UTF-8'))
+            print(response.read().decode('UTF-8'))
     except error.HTTPError as er:
         print("Error code:", er.code)
