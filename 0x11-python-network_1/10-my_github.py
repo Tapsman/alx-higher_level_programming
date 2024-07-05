818a79f903204ba1837c2ae37fd102cbb297c368
Tapsman <tapiwanyoni140@gmail.com>
2024-07-05 13:04:01 -0700

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script will take Github credentials and uses API to the id"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    read = requests.get("https://api.github.com/Tapsman", auth=auth)
+    print(read.json().get("id"))
