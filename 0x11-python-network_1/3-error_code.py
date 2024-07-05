5e4f59123d453252edf3df162749bdbb723ef6e6
Tapsman <tapiwanyoni140@gmail.com>
2024-07-05 08:38:46 -0700

    3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script will take URL, sends request and then displays body of response"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read()decode('UTF-8'))
+    except error.HTTPError as er:
+        print("Error code:", er.code)
