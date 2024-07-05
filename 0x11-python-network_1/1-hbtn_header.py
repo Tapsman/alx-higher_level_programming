1df79b7a72c028d64d0c4206aa861d3d459c6027
Tapsman <tapiwanyoni140@gmail.com>
2024-07-05 05:53:26 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-import sys
-import urllib.request
+#!/usr/bin/python
 """
 This is a python script that will:
     - Take in the URL
@@ -8,6 +6,8 @@ This is a python script that will:
     - And then displays the value of the X-Request-ID
     - Of the variable that is found on the header of the response
 """
+import sys
+import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
