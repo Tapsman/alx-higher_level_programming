77dd3fa184bfbd01bafaabfe11143dc7ef0e6c1a
Tapsman <tapiwanyoni140@gmail.com>
2024-07-07 11:46:07 -0700

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python script that takes 2 args on recent commits to github repo"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    com = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                com[i].get("sha"),
+                com[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
