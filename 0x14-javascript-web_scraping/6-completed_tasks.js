470010143b519fc96dde844002087876fb9372ed
Tapsman <tapiwanyoni140@gmail.com>
2024-07-30 13:29:45 -0700

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// Script that will compute the number of taskes completed
+// Where the first argument is API url
+//
+const request = require('request');
+
+request.get(process.argv[2], { json: true }, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] = tasksCompleted[todo.userId] + 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
+});
