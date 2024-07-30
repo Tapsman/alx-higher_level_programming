031b33c81eb64347c316e2f9352e97b74ccd6a2a
Tapsman <tapiwanyoni140@gmail.com>
2024-07-30 12:34:06 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => characters.endsWith('/18/'))
+      return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
     }, 0));
