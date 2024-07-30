b5c7e39081931143df3eb05b89c33a000e52cf87
Tapsman <tapiwanyoni140@gmail.com>
2024-07-30 12:50:03 -0700

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,7 +2,5 @@
 // This is a script that will get the content a webpage and stores in a file.
 
 const fs = require('fs');
-
 const request = require('request');
-
 request(process.argv[2].pipe(fs.createWriteStream(process.argv[3]));
