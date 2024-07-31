87a19d8c4e6c9f8cb686156e87ec0f605772b662
Tapsman <tapiwanyoni140@gmail.com>
2024-07-31 11:02:53 -0700

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,7 +4,7 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const fleStream = fs.createWriteStream(process.argv[3]);
+const fileStream = fs.createWriteStream(process.argv[3]);
 
 request
   .get(url)
