3b8fc35ddf68c35f714340bca6e03fc468eadc81
Tapsman <tapiwanyoni140@gmail.com>
2024-08-10 13:09:59 -0700

    101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// This is a script that imports a dictionary
+
+const { dict } = require('./101-data');
+const convertedArr = Object.entries(dict);
+const newObj = {};
+
+convertedArr.forEach(element => {
+  newObj[element[1]] ? newObj[element[1]].push(element[0]) : newObj[element[1]] = [element[0]];
+});
+
+console.log(newObj);
