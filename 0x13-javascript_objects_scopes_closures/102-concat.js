5ee428c94854ff20d14bdf96d4e1a24e24dca6d0
Tapsman <tapiwanyoni140@gmail.com>
2024-08-10 13:00:26 -0700

    102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// This is a script that concatenates two files
+
+const fs = require('fs').promise
+const { argv } = require('process');
+
+fs.readFile(argv[2], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, 'utf8'))
+  .catch(err => console.error(err));
+
+fs.readFile(argv[3], 'utf8')
+  .then(data => fs.writeFile(argv[4], data, { flag: 'a' }, 'utf8'))
+  .catch(err => console.error(err));
